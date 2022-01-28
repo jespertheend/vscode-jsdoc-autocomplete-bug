1227d2e3a3849faa851b21537051d6752104aec4
Jesper van den Ende <jespertheend@users.noreply.github.com>
2022-01-28 10:46:41 +0100

    Add jsdoc.js

---
+++ jsdoc.js
@@ -0,0 +1,17 @@
+// Place your caret here and type [*] [Enter]
+// ||
+// \/
+  /*
+
+/**
+ * @typedef FooObj
+ * @property {number} num
+ */
+
+/**
+ * @param {FooObj} a
+ * @param {FooObj} b
+ */
+ function foo(a, b) {
+    return a.num + b.num;
+}
